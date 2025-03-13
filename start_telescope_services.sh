259ab77a4a386c547bf0e11c2c733b63d5808d5f
Navanith Niranjan <navanith.niran@gmail.com>
2025-03-12 20:59:42 -0400

    modified bash script

--- start_telescope_services.sh
+++ start_telescope_services.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+
+# Activate virtual environment
+cd /home/pi/repos/fydp-smart-telescope-mount
 source venv/bin/activate
 
+# Running calibrate.py
 echo "Running calibrate.py..."
-cd astap/
+cd /home/pi/repos/fydp-smart-telescope-mount/astap
 python3 calibrate.py & 
 
+# Starting FastAPI server
 echo "Starting FastAPI server..."
-cd ../web-server
+cd /home/pi/repos/fydp-smart-telescope-mount/web-server
 uvicorn server:app --host 0.0.0.0 --port 8000 &
 
+# Starting Vite React frontend
 echo "Starting Vite React frontend..."
-cd ../web-client
+cd /home/pi/repos/fydp-smart-telescope-mount/web-client
 npm run dev -- --host 0.0.0.0 
